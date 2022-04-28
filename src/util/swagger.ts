00a9ff96ce6a99d389700945bd79c80696c38fec
vvhustle <vvhustle@gmail.com>
2022-04-28 15:38:48 +0900

    Fix Swagger Title Text

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -8,8 +8,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs')
-    .setDescription('NestJS Study API description')
+    .setTitle('SanNumSan-SERVER API Docs with Swagger')
+    .setDescription('SanNumSan-SERVER API description')
     .setVersion('1.0.0')
     .build();
 
