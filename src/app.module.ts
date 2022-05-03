30e03d2f120c8fbb546829baf56e60437b34dfef
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-05-03 22:03:58 +0900

    feat: kakao login 플로우만

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { AuthModule } from './auth/auth.module';
+import { Auth } from './auth';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [UserController, AuthController],
+  providers: [UserService, AuthService],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-kakao';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '../user/user.entities';
+import fetch from 'cross-fetch';
+import { KakaoIdDto, KakaoTokenDto } from './dto/auth.kakao.dto';
+
+@Injectable()
+export class AuthService {
+  private users: User[] = [];
+
+  getAll(): User[] {
+    return this.users;
+  }
+
+  getOne(id: number): User {
+    const user: User = this.users.find((user) => user.id === Number(id));
+    if (!user) {
+      throw new NotFoundException(`user id ${id} not found`);
+    }
+    return user;
+  }
+
+  async getKakaoAccessToken(
+    code: string,
+    client_id: string,
+    redirect_uri: string,
+  ): Promise<KakaoTokenDto> {
+    return await fetch(
+      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      },
+    ).then((res) => res.json());
+  }
+
+  async getKakaoUserCode(access_token: string): Promise<KakaoIdDto> {
+    return await fetch('https://kapi.kakao.com/v2/user/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }).then((res) => res.json());
+  }
+
+  getUserByUserCode(usercode): User {
+    const user: User = this.users.find((user) => user.code === usercode.id);
+    if (!user) {
+      throw new NotFoundException(
+        `user code ${usercode.id} not found, register requested`,
+      );
+    }
+    return user;
+  }
+
+  register(userData): User {
+    this.users.push({
+      id: this.users.length + 1,
+      name: 'test',
+      code: userData.id,
+      provider: 'kakao',
+    });
+    return userData;
+  }
+}
