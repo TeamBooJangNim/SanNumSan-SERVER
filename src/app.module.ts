2d4be67d1d60483a261497fc69c7fe9f6dbb2b4c
Wonhee Jeong <vvhustle@gmail.com>
2022-05-14 15:06:06 +0900

    AppMoudule에 게시글 POST API 관련 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,29 @@ import { AuthModule } from './auth/auth.module';
 import { Auth } from './auth';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FirestoreModule } from './firestore/firestore.module';
+
+import { PostModule } from './post/post.module';
+import { PostController } from './post/post.controller';
+import { PostService } from './post/post.service';
 
 @Module({
-  imports: [],
-  controllers: [UserController, AuthController],
-  providers: [UserService, AuthService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    FirestoreModule.forRoot({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        keyFilename: configService.get<string>('SA_KEY'),
+        projectId: configService.get<string>('PROJECT_ID'),
+      }),
+      inject: [ConfigService],
+    }),
+    PostModule,
+  ],
+  controllers: [UserController, AuthController, PostController],
+  providers: [UserService, AuthService, PostService],
 })
 export class AppModule {}
